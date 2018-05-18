311a65777d9e0f94a99a65370acf5d8d461e705c
Annie Schaefer <annietschaefer@outlook.com>
2018-05-18 13:01:26 -0400

    Post

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -3,25 +3,49 @@ import { Col, Row, Wrapper } from "../BootstrapGrid";
 import "./Post.css"; // User Post CSS
 
 // Luis: Starter Code
-const Post = () => (
+const Post = ({ image, name, category, timeStamp, comment }) => (
 
-    <Wrapper>
 
-        <h1>User Post</h1>
-        <br />
-        <h4>Route: '/feed' AND '/profile'</h4>
-        <h4>Rendering User Post:
-            <ol>
-                <li>User Image</li>
-                <li>Name</li>
-                <li>Post Timestamp</li>
-                <li>Category</li>
-                <li>Comment</li>
-            </ol>
-        </h4>
 
-    </Wrapper>
+    // {/* <h1>User Post</h1>
+    //     <br />
+    //     <h4>Route: '/feed' AND '/profile'</h4>
+    //     <h4>Rendering User Post:
+    //         <ol>
+    //             <li>User Image</li>
+    //             <li>Name</li>
+    //             <li>Post Timestamp</li>
+    //             <li>Category</li>
+    //             <li>Comment</li>
+    //         </ol>
+    //     </h4> */}
+    <div>
+        <Row>
+            <Col size="md" span="6">
+                <div className="panel post-panel">
+                    <div className="panel-body">
+                        <Row>
+                            <Col size="md" span="2">
+                                <img src={image} alt={name} />
+                            </Col>
+                            <Col size="md" span="5">
+                                {name}
+                            </Col>
+                            </Row>
 
-);
+                            {timeStamp}
+                            <div className="panel-body">
+                                {category}
+                                <br />
+                                {comment}
 
+                            </div>
+                </div>
+                </div>
+            </Col>
+        </Row>
+    </div>
+
+            );
+            
 export default Post;
--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { Col, Row, Wrapper } from "../../components/BootstrapGrid";
 import "./Feed.css"; // Feed Page CSS
+import Post from "../../components/Post";
+import CatButton from "../../components/CatButton";
+import PostForm from "../../components/PostForm";
 
 // Luis: Starter Code
 const Feed = () => (
 
     <Wrapper>
 
-        <h1>Feed Page</h1>
+        {/* <h1>Feed Page</h1>
         <br />
         <h4>Route: '/feed'</h4>
         <h4>Rendering:
             <ol>
-                <li>Post Category Buttons</li>
-                <li>Feed with all user posts</li>
-                <li>New Post Form</li>
+                <li>  </li>
+                <li>  </li>
+                <li>  </li>
             </ol>
-        </h4>
+        </h4> */}
+
+        <Post 
+        image="https://www.publicdomainpictures.net/pictures/30000/velka/portrait-of-a-man-1331296473U9x.jpg"
+        name="Mr Man"
+        category="Traffic"
+        comment="No comment"
+        timeStamp="date"
+        />
+
 
     </Wrapper>
 
