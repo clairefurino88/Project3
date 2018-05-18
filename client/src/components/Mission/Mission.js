01d4af69d1b2144f7178ac589792250403568b76
LMorel77 <lmorel77@aol.com>
2018-05-18 14:54:43 -0400

    Updated Home Page

---
+++ client/src/components/Mission/Mission.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Col, Row, Wrapper } from "../BootstrapGrid";
+import "./Mission.css"; // Mission Statement CSS
+
+// Luis: Starter Code
+const Mission = () => (
+
+    <div>
+
+        <center><h2 class="mission">Our Mission</h2> </center>
+        <p>Hate&ampDate is an application to work under the principle that mutual dislikes bring people closer than shared interests.
+        Almost every dating app collects information regarding people's passions and hobbies. However, even if you and
+        a 'match' love many of the same things, if you disagree on one thing you hate, your match would be ruined. So instead,
+        why not discuss what you hate? This application allows people to share stories in ten different categories about
+        why they hate or what they hate about each category —including anything from Donald Trump to Traffic on a Monday
+        Morning — using a blog forum. Hate&ampDate is a new and refreshing way to find a real match. Hate&ampDate is making
+        online dating more approachable by replacing cumbersome surveys and bios with a fun, alternative way to express
+        your personality.
+        </p>
+        {/* <p>
+            <center>
+                <img class="img-con" src="/img/hnd_con.png" alt="logo" />
+            </center>
+        </p> */}
+
+    </div>
+
+);
+
+export default Mission;
--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import {Col, Row, Wrapper} from "../BootstrapGrid"
+import { Col, Row, Wrapper } from "../BootstrapGrid"
 import "./SignUpForm.css"; // Sign-Up Form CSS
 import API from "../../utils/API";
 
@@ -13,61 +13,123 @@ const styles = {
   }
 }
 
-class SignUpForm extends Component {
-  state = {
-    redirectTo:"",
-    email: "",
-    password: ""
-  }
+const SignUpForm = ({
+  handleInputChange,
+  handleSignUp,
+  name,
+  email,
+  password,
+  occupation,
+  relationshipType,
+  location,
+  imageURL,
+  bio
+}) => {
 
-  handleInputChange = event => {
-    const {name, value} = event.target;
-    this.setState({[name]: value})
-  };
+  return (
 
-  handleSignUp = event => {
-    event.preventDefault();
-    API.signUp({ email: this.state.email, password: this.state.password})
-    .then(() => this.setState({
-      redirectTo: "/login"
-    }));
-  }
+    <div>
+      <h1>Signup</h1>
+      <form className="signup" onSubmit={this.handleSignup}>
+        <div className="form-group">
+          <label for="name">Name</label>
+          <input name="name" className="form-control" placeholder="Enter your name..." onChange={this.handleInputChange} value={name} type="name" required />
+        </div>
+        <div className="form-group">
+          <label for="email">E-mail</label>
+          <input name="email" className="form-control" placeholder="Enter your e-mail..." onChange={this.handleInputChange} value={email} type="email" required />
+        </div>
+        <div className="form-group">
+          <label for="password">Password</label>
+          <input name="password" className="form-control" placeholder="Enter a password..." onChange={this.handleInputChange} value={password} type="password" required />
+        </div>
+        <div className="form-group">
+          <label for="occupation">Occupation</label>
+          <input name="occupation" className="form-control" placeholder="Enter your occupation..." onChange={this.handleInputChange} value={occupation} type="text" required />
+        </div>
+        <div className="form-group">
+          <label for="relationshipType">Relationship Type</label>
+          <input name="relationshipType" className="form-control" placeholder="What kind of relationship are you looking for?" onChange={this.handleInputChange} value={relationshipType} type="text" required />
+        </div>
+        <div className="form-group">
+          <label for="location">Location</label>
+          <input name="location" className="form-control" placeholder="Location..." onChange={this.handleInputChange} value={location} type="text" required />
+        </div>
+        <div className="form-group">
+          <label for="imageUrl">Profile Picture</label>
+          <input name="imageUrl" className="form-control" placeholder="Image Url..." onChange={this.handleInputChange} value={imageURL} type="text" required />
+        </div>
+        <div className="form-group">
+          <label for="bio">About You</label>
+          <textarea name="bio" className="form-control" placeholder="Say something about yourself..." onChange={this.handleInputChange} value={bio} type="text" required />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
 
-  render(){
-    if(this.state.redirectTo){
-      return <Redirect to={this.state.redirectTo} />
-    }
-    return (
-      <Wrapper>
-        <Row>
-          <Col>
-            <h1 style={styles.header}>Signup Form</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={2} offset={3}>
-            <label>Email: </label>
-          </Col>
-          <Col span={3}>
-            <input name="email" type="text" value={this.state.email} onChange={this.handleInputChange} />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={2} offset={3}>
-            <label>Password: </label>
-          </Col>
-          <Col span={4}>
-            <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={2} offset={3}>
-            <button onClick={this.handleSignUp}>Submit</button>
-          </Col>
-        </Row>
-      </Wrapper>
-    )
-  }
-}
+  );
+
+};
 
 export default SignUpForm;
+
+// Original Franklin Starter Code...
+
+// class SignUpForm extends Component {
+//   state = {
+//     redirectTo:"",
+//     email: "",
+//     password: ""
+//   }
+
+//   handleInputChange = event => {
+//     const {name, value} = event.target;
+//     this.setState({[name]: value})
+//   };
+
+//   handleSignUp = event => {
+//     event.preventDefault();
+//     API.signUp({ email: this.state.email, password: this.state.password})
+//     .then(() => this.setState({
+//       redirectTo: "/login"
+//     }));
+//   }
+
+//   render(){
+//     if(this.state.redirectTo){
+//       return <Redirect to={this.state.redirectTo} />
+//     }
+//     return (
+//       <Wrapper>
+//         <Row>
+//           <Col>
+//             <h1 style={styles.header}>Signup Form</h1>
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col span={2} offset={3}>
+//             <label>Email: </label>
+//           </Col>
+//           <Col span={3}>
+//             <input name="email" type="text" value={this.state.email} onChange={this.handleInputChange} />
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col span={2} offset={3}>
+//             <label>Password: </label>
+//           </Col>
+//           <Col span={4}>
+//             <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col span={2} offset={3}>
+//             <button onClick={this.handleSignUp}>Submit</button>
+//           </Col>
+//         </Row>
+//       </Wrapper>
+//     )
+//   }
+// }
+
+// export default SignUpForm;
