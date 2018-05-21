8bf5bf103215ff8450f81ae996a9ff22b4a84969
Annie Schaefer <annietschaefer@outlook.com>
2018-05-21 12:50:21 -0400

    profile page styling

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -3,63 +3,34 @@ import { Col, Row, Wrapper } from "../BootstrapGrid";
 import "./UserInfo.css"; // Profile Page User Info CSS
 
 // Luis: Starter Code
-const UserInfo = ({image, name, email, occupation, relationshipType, location, bio}) => (
+const UserInfo = ({ image, name, email, occupation, relationshipType, location, bio }) => (
 
-    <Wrapper>
-        <div style={{
-        backgroundColor:'grey', 
-        borderRadius:'2%'}} className="userDiv">
+        <div className="userDiv">
+
+        <div className="userTitle">About Me</div>
         <br />
-        <strong style={{
-        paddingLeft:'5px', 
-        fontSize:'14px', 
-        color:'white',
-        background:'linear-gradient(to bottom right, #e1000e, black)',
-        display:'block',
-        boxShadow:'0 2px 4px 0 rgba(38, 47, 42, 1), 0 6px 15px 0 rgba(0, 0, 0, 0.19',
-        padding:'10px 15px',
-        borderBottom:'1px solid transparent',
-        borderTopRightRadius:'3px',
-        borderTopLeftRadius:'3px'}}>About Me</strong>
-        <br />
-        <br />
-        <img style={{ 
-        maxWidth:'300px', 
-        maxHeight:'300px', 
-        minWidth:'300px', 
-        minHeight:'300px', 
-        display:'block', 
-        margin:'auto',
-        border:'3px solid #ffffff',
-        boxShadow:'3-x 4px',
-        borderRadius:'0% 10% 0% 10%'}} src={image} />
-        <br />
-        <br />
-        <strong style={{
-        paddingLeft:'5px'}}>Name:</strong> <p style={{color:'white', 
-        paddingLeft:'5px', marginBottom:'0', fontSize:'17px'}}>{name}</p>
-        <br />
-        <strong style={{
-        paddingLeft:'5px'}}>Email:</strong> <p style={{color:'white', 
-        paddingLeft:'5px', marginBottom:'0', fontSize:'17px'}}>{email}</p>
-        <br />
-        <strong style={{
-        paddingLeft:'5px'}}>Occupation:</strong> <p style={{color:'white', 
-        paddingLeft:'5px', marginBottom:'0', fontSize:'17px'}}>{occupation}</p>
-        <br />
-        <strong style={{
-        paddingLeft:'5px'}}>Looking for:</strong> <p style={{color:'white', 
-        paddingLeft:'5px', marginBottom:'0', fontSize:'17px'}}>{relationshipType}</p>
-        <br />
-        <strong style={{
-        paddingLeft:'5px'}}>Location:</strong> <p style={{color:'white', 
-        paddingLeft:'5px', marginBottom:'0', fontSize:'17px'}}>{location}</p>
-        <br />
-        <strong style={{
-        paddingLeft:'5px'}}>Bio:</strong> <p style={{color:'white', 
-        paddingLeft:'5px', marginBottom:'0', fontSize:'17px'}}>{bio}</p>
-        </div>
-        {/* <h1>Profile Page User Info</h1>
+        
+        <img className="userImg" src={image} />
+
+        <p className="userInfo" > Name: </p> <p className="userData"> {name} </p>
+
+        <p className="userInfo" > Email: </p> <p className="userData"> {email} </p>
+
+        <p className="userInfo" > Occupation: </p> <p className="userData">{occupation} </p>
+
+        <p className="userInfo" > Relationship Type: </p> <p className="userData">{relationshipType}</p>
+
+        <p className="userInfo" > Location: </p> <p className="userData">{location}</p>
+
+        <p className="userInfo" > Bio: </p> <p className="userData">{bio}</p>
+
+    </div>
+
+);
+
+export default UserInfo;
+
+{/* <h1>Profile Page User Info</h1>
         <br />
         <h4>Route: '/profile'</h4>
         <h4>Rendering Logged On User Info:
@@ -73,9 +44,3 @@ const UserInfo = ({image, name, email, occupation, relationshipType, location, b
                 <li>Bio</li>
             </ol>
         </h4> */}
-
-    </Wrapper>
-
-);
-
-export default UserInfo;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import DevCard from "../../components/DevCard";
 import UserInfo from "../../components/UserInfo";
+import Post from "../../components/Post";
 import { Col, Row, Wrapper } from "../../components/BootstrapGrid";
 import "./Profile.css"; // Profile Page CSS
 
@@ -9,7 +10,41 @@ const Profile = () => (
 
     <Wrapper>
 
-        {/* <h1>Profile Page</h1>
+        <Row>
+            {/*  User Information Column */}
+            <Col size="md" span="7">
+                {/*  User Image */}
+
+                <UserInfo
+                    image="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Flmcdesign-rj6zcy7b8ypu79snuv.netdna-ssl.com%2Fwp-content%2Fuploads%2F2016%2F01%2FShrek_Tile-1-500x500.jpg&f=1"
+                    name="Bobby Billy-Boy"
+                    email="bobbybillyboy@gmail.com"
+                    occupation="Plumber"
+                    relationshipType="Long-term love"
+                    location="Newark, NJ"
+                    bio="I'm just a regular plumber, dawg." />
+
+
+                {/* Empty Column */}
+                <Col size="md" span="1"> </Col>
+
+                <Col size="md" span="4">
+                    <Post
+                        comment="User's feed post"
+                        category="Traffic"
+                    />
+                </Col>
+            </Col>
+
+        </Row>
+
+    </Wrapper>
+
+);
+
+export default Profile;
+
+{/* <h1>Profile Page</h1>
         <br />
         <h4>Route: '/profile'</h4>
         <h4>Rendering:
@@ -18,18 +53,3 @@ const Profile = () => (
                 <li>All user posts</li>
             </ol>
         </h4> */}
-
-    <UserInfo 
-    image="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Flmcdesign-rj6zcy7b8ypu79snuv.netdna-ssl.com%2Fwp-content%2Fuploads%2F2016%2F01%2FShrek_Tile-1-500x500.jpg&f=1" 
-    name="Bobby Billy-Boy"
-    email="bobbybillyboy@gmail.com"
-    occupation="Plumber"
-    relationshipType="Long-term love"
-    location="Newark, NJ"
-    bio="I'm just a regular plumber, dawg."/>
-
-    </Wrapper>
-
-);
-
-export default Profile;
