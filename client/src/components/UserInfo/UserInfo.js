86b7202a6040e12b67f1554aa293c64204afcc50
Annie Schaefer <annietschaefer@outlook.com>
2018-05-21 16:38:57 -0400

    styled userInfo page

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -1,32 +1,30 @@
 import React from "react";
-// import { Col, Row, Wrapper } from "../BootstrapGrid";
-import { Wrapper } from "../BootstrapGrid";
+import { Col, Row, Wrapper } from "../BootstrapGrid";
+//import { Wrapper } from "../BootstrapGrid";
 import "./UserInfo.css"; // Profile Page User Info CSS
 
 
 // Luis: Starter Code
 const UserInfo = ({ image, name, email, occupation, relationshipType, location, bio }) => (
 
-        <div className="userDiv">
+    <Row>
+        <Col size="md" span="6">
+            <br />
+            <img className="userImg" src={image} />
+        </Col>
+        <Col size="md" span="6">
+            <br />
+            <p className="userInfo" > Name: </p> <p className="userData">{name}</p>
+            <p className="userInfo" > Email: </p> <p className="userData">{email}</p>
+            <p className="userInfo" > Occupation: </p> <p className="userData">{occupation} </p>
+            <p className="userInfo" > Relationship Type: </p> <p className="userData">{relationshipType}</p>
+            <p className="userInfo" > Location: </p> <p className="userData">{location}</p>
+            <p className="userInfo" > Bio: </p> <p className="userData">{bio}</p>
+        </Col>
+    </Row>
 
-        <div className="userTitle">About Me</div>
-        <br />
-        
-        <img className="userImg" src={image} />
 
-        <p className="userInfo" > Name: </p> <p className="userData"> {name} </p>
 
-        <p className="userInfo" > Email: </p> <p className="userData"> {email} </p>
-
-        <p className="userInfo" > Occupation: </p> <p className="userData">{occupation} </p>
-
-        <p className="userInfo" > Relationship Type: </p> <p className="userData">{relationshipType}</p>
-
-        <p className="userInfo" > Location: </p> <p className="userData">{location}</p>
-
-        <p className="userInfo" > Bio: </p> <p className="userData">{bio}</p>
-
-    </div>
 
 
 );
