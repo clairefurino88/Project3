6008f4fb1b87f7fe6255cadef6170efa88416399
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-05-21 13:02:37 -0400

    fixed about page errors

--- client/src/components/CatButton/CatButton.js
+++ client/src/components/CatButton/CatButton.js
@@ -5,56 +5,33 @@ import "./CatButton.css"; // Category Button CSS
 
 // Luis: Starter Code
 const CatButton = () => (
-<nav className="navbar navbar-expand-lg navbar-light bg-light" id="catButton">
-<a className="navbar-brand" id="catBar">
-<div>
-    <ul className="navbar-nav" id="categoryButtons">
-<li>All</li>
-<li>Coding</li>
-<li>Education</li>
-<li>Education</li>
-<li>Fashio</li>
-<li>Food</li>
-<li>Health</li>
-<li>Love</li>
-<li>Money</li>
-<li>People</li>
-<li>Politics</li>
-<li>Teens</li>
-<li>Transportation</li>
-<li>Weather</li>
-</ul>
-</div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light" id="catButton">
+        <a className="navbar-brand" id="catBar">
+            <div>
+                <ul className="navbar-nav" id="categoryButtons">
+                    <li>All</li>
+                    <li>Coding</li>
+                    <li>Education</li>
+                    <li>Education</li>
+                    <li>Fashio</li>
+                    <li>Food</li>
+                    <li>Health</li>
+                    <li>Love</li>
+                    <li>Money</li>
+                    <li>People</li>
+                    <li>Politics</li>
+                    <li>Teens</li>
+                    <li>Transportation</li>
+                    <li>Weather</li>
+                </ul>
+            </div>
 
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" id="catButton">
-            <a className="navbar-brand" id="categoryButtons">
-                <div>
-                    <ul className="navbar-nav">
-                        <li>All</li>
-                        <li>Coding</li>
-                        <li>Education</li>
-                        <li>Education</li>
-                        <li>Fashio</li>
-                        <li>Food</li>
-                        <li>Health</li>
-                        <li>Love</li>
-                        <li>Money</li>
-                        <li>People</li>
-                        <li>Politics</li>
-                        <li>Teens</li>
-                        <li>Transportation</li>
-                        <li>Weather</li>
-                    </ul>
-                </div>
-
-            </a>
-        </nav>
+        </a>
+    </nav>
 
 
-    );
+);
 
-};
 
 export default CatButton;
