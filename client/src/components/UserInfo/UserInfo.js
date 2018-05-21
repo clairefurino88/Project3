e121d8268fdb45c530b83be1879f9af58f059157
Annie Schaefer <annietschaefer@outlook.com>
2018-05-21 16:57:39 -0400

    adjusted feed column in UserInfo page

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -1,29 +1,23 @@
 import React from "react";
 // import { Col, Row, Wrapper } from "../BootstrapGrid";
-import { Wrapper } from "../BootstrapGrid";
+//import { Wrapper } from "../BootstrapGrid";
 import "./UserInfo.css"; // Profile Page User Info CSS
 
 
 // Luis: Starter Code
 const UserInfo = ({ image, name, email, occupation, relationshipType, location, bio }) => (
 
-        <div className="userDiv">
+    <div className="userDiv">
 
         <div className="userTitle">About Me</div>
         <br />
-        
-        <img className="userImg" src={image} />
 
+        <img className="userImg" src={image} alt="userprofilephoto" />
         <p className="userInfo" > Name: </p> <p className="userData"> {name} </p>
-
         <p className="userInfo" > Email: </p> <p className="userData"> {email} </p>
-
         <p className="userInfo" > Occupation: </p> <p className="userData">{occupation} </p>
-
         <p className="userInfo" > Relationship Type: </p> <p className="userData">{relationshipType}</p>
-
         <p className="userInfo" > Location: </p> <p className="userData">{location}</p>
-
         <p className="userInfo" > Bio: </p> <p className="userData">{bio}</p>
 
     </div>
@@ -32,18 +26,3 @@ const UserInfo = ({ image, name, email, occupation, relationshipType, location, 
 );
 
 export default UserInfo;
-
-{/* <h1>Profile Page User Info</h1>
-        <br />
-        <h4>Route: '/profile'</h4>
-        <h4>Rendering Logged On User Info:
-            <ol>
-                <li>Image</li>
-                <li>Name</li>
-                <li>Email</li>
-                <li>Occupation</li>
-                <li>Relationship Type</li>
-                <li>Location</li>
-                <li>Bio</li>
-            </ol>
-        </h4> */}
