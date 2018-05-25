f7c19bd067c6089c4047c8dc99e6a640b53a2528
LMorel77 <lmorel77@aol.com>
2018-05-25 09:56:09 -0400

    Rebranding

--- client/src/components/Mission/Mission.js
+++ client/src/components/Mission/Mission.js
@@ -7,15 +7,15 @@ const Mission = () => (
     <div className="mission">
 
         <h1 className="missionHeader">Our Mission</h1>
-        <p className="missionStatement">Hate&Date is an application that works under
+        <p className="missionStatement">Gripe is an application that works under
         the principle that mutual dislikes bring people closer than shared interests. Almost
         every dating app collects information regarding people's passions and hobbies. However,
         even if you and a 'match' love many of the same things, if you disagree on one thing
         you hate, your match would be ruined. So instead, why not discuss what you hate? This
         application allows people to share stories in ten different categories about why they
         hate or what they hate about each category —including anything from Donald Trump to
-        Traffic on a Monday Morning — using a blog forum. Hate&Date is a new and refreshing way
-        to find a real match. Hate&ampDate is making online dating more approachable by replacing
+        Traffic on a Monday Morning — using a blog forum. Gripe is a new and refreshing way
+        to find a real match. Gripe is making online dating more approachable by replacing
         cumbersome surveys and bios with a fun, alternative way to express your personality.
         </p>
         {/* <p>
