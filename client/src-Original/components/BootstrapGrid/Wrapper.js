d7dc3d8cd03193c72e56f117b7de77048f066f21
LMorel77 <lmorel77@aol.com>
2018-05-25 00:03:33 -0400

    Cleanup and Heroku Update

--- client/src-Original/components/BootstrapGrid/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Wrapper = ({children}) => (
-  <div className="container">
-    {children}
-  </div>
-);
-
-export default Wrapper;
--- client/src-Original/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Home } from "./Home";
-export { default as SignupForm } from "./SignupForm";
-export { default as Login } from "./Login";
